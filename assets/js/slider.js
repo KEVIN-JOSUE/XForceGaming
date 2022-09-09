284ffdc68bafd120d06e7a38bb43791ff91f0bb8
KEVIN-JOSUE <71499421@certus.edu.pe>
2022-09-09 02:14:43 -0500

    primer commit

---
+++ assets/js/slider.js
@@ -0,0 +1,43 @@
+let slider = document.querySelector('.contenedor_slider');
+let cantidad_diapositivas = document.querySelectorAll('.contenedor_slider ul li').length;
+let inicio = 1;
+let direccion = 'siguiente';
+
+
+const siguiente = () => {
+    slider.style.marginLeft = '';
+};
+
+
+const anterior = () => {
+    slider.style.marginLeft = '';
+};
+
+
+const mover = () => {
+    slider.style.marginLeft = `-${inicio * 100}%`;
+};
+
+
+const automatico = () => {
+    console.log(inicio);
+
+    if(inicio === cantidad_diapositivas - 1){
+        direccion = 'anterior';
+    }
+    
+    if(inicio === 0){
+        direccion = 'siguiente';
+    }
+
+    if(direccion === 'siguiente'){
+        mover();
+        inicio++;
+    }else{
+        mover();
+        inicio--;
+    }
+};
+
+
+setInterval(automatico, 10000);
